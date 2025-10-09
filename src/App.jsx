26a8825d4a3ef270ee60872f4aa87f95bbba4548
Petra-78 <partos.petra.0305@gmail.com>
2025-10-09 18:01:37 +0200

    Add the parent states for the education section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import PersonalDataForm from "./components/Personal.jsx";
 import PersonalDataDisplay from "./components/PersonalDisplay.jsx";
 import "./App.css";
+import EducationDropdown from "./components/Education.jsx";
 
 function App() {
   const [personalData, setPersonalData] = useState({
@@ -11,6 +12,15 @@ function App() {
     address: "",
   });
   const [personalForm, setPersonalForm] = useState(personalData);
+
+  const [educationData, setEducationData] = useState({
+    schoolName: "",
+    degree: "",
+    startDate: "",
+    endDate: "",
+    location: "",
+  });
+  const [educationForm, setEducationForm] = useState(educationData);
   return (
     <>
       <PersonalDataForm
@@ -20,6 +30,7 @@ function App() {
         setPersonalForm={setPersonalForm}
       />
       <PersonalDataDisplay personalForm={personalForm} />
+      <EducationDropdown />
     </>
   );
 }
