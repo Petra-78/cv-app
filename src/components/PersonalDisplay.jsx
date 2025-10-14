5c2805949777de0bbe2576dcf1d89abd2909ffcd
Petra-78 <partos.petra.0305@gmail.com>
2025-10-14 15:51:38 +0200

    Add fontawesome icons

--- src/components/PersonalDisplay.jsx
+++ src/components/PersonalDisplay.jsx
@@ -1,17 +1,39 @@
-import App from "../App";
 import "../styles/Personal.css";
+import ReactDOM from "react-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 export default function PersonalDataDisplay({ personalForm }) {
   return (
     <>
       <div className="personalDetails">
         <h2>{personalForm.fullName}</h2>
-        <p>{personalForm.email}</p>
-        <p>{personalForm.phone}</p>
-        <p>{personalForm.address}</p>
+        <p>
+          {personalForm.email && (
+            <FontAwesomeIcon icon={faEnvelope} style={{ marginRight: "8px" }} />
+          )}
+          {personalForm.email}
+        </p>
+        <p>
+          {personalForm.phone && (
+            <FontAwesomeIcon icon={faPhone} style={{ marginRight: "8px" }} />
+          )}
+          {personalForm.phone}
+        </p>
+        <p>
+          {personalForm.address && (
+            <FontAwesomeIcon
+              icon={faLocationDot}
+              style={{ marginRight: "8px" }}
+            />
+          )}
+          {personalForm.address}
+        </p>
       </div>
       <div className="introduction">
-        <h2>Introduction</h2>
+        <h2 className="sectionTitle">Introduction</h2>
         <p className="introduction">{personalForm.introduction}</p>
       </div>
     </>
